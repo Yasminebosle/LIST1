8af5c573864cf55442ca66959f54f55f41b047c3
yasminebosle <fareedahusman@ryanacademy.shc.ng>
2024-12-28 10:18:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+emty_list = []
+print()
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+
+
+triples = [1,2,3,4,5,6,7,8]*2
+print(triples)
+
+
+aList = [100,200,300,400,500,600,700,800,900.1000]
+aList = aList[::-1]
+print(aList,"\n")
+
